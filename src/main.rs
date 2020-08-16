642da7ac7a010a0e4ec6276c66ec8544b56f0b7c
patrickvonplaten <patrick.v.platen@gmail.com>
2020-08-17 00:43:18 +0200

    add gaussian struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 use std::fs::File;
 use std::str::FromStr;
 use std::process;
+use rand::prelude::*;
+use rand_distr::StandardNormal;
 
 fn main() {
     let mut data:Vec<Vec<f32>> = vec![];
@@ -10,7 +12,9 @@ fn main() {
         process::exit(1);
     }
     
-    println!("The data {:?}", data)
+    // println!("The data {:?}", data);
+    let gauss_prob: GaussianProb = GaussianProb::new(4);
+    println!("Random gauss {:?}", gauss_prob);
 }
 
 fn read_in_csv(data: &mut Vec<Vec<f32>>, csv_file_name: &str) -> Result<(),Box<dyn Error>> {
@@ -31,6 +35,52 @@ fn read_in_csv(data: &mut Vec<Vec<f32>>, csv_file_name: &str) -> Result<(),Box<d
     Ok(())
 }
 
+
+/* pub struct GaussianMixture {
+    /// gaussian probability distributions
+    gaussian_probs: Vec<GaussionProb>,
+    /// probs weights
+    probs_weights: Vec<f32>,
+}
+
+*/
         
 
+#[derive(Debug)]
+pub struct GaussianProb {
+    /// mean
+    mean: Vec<f32>,
+    /// variance
+    variance: Vec<f32>,
+    ///
+    dim: u32,
+}
+impl GaussianProb {
+    pub fn new(
+        dim: u32
+    ) -> Self {
+        let mean: Vec<f32> = get_random_vector(dim);
+        let variance: Vec<f32> = get_random_vector(dim);
+        GaussianProb {
+            mean,
+            variance,
+            dim
+        }
+    }
+    pub fn from_mean_and_variance(mean: Vec<f32>, variance: Vec<f32>) -> Self {
+        let dim = mean.len() as u32;
+        GaussianProb {
+            mean,
+            variance,
+            dim
+        }
+    }
+}
 
+
+fn get_random_vector(dim: u32) -> Vec<f32> {
+    let random_vec: Vec<f32> = (0..dim).map(|_| {
+        thread_rng().sample(StandardNormal)
+        }).collect();
+    random_vec
+}
